5832d0afcead72014b79d9025f4be681c23bb3ce
muhammad_wael <mwael1971@gamil.com>
2024-07-03 21:08:52 +0300

    solve the sequence problem in clinic appointment

--- models/clinic_appointment.py
+++ models/clinic_appointment.py
@@ -4,7 +4,6 @@ from odoo.exceptions import ValidationError,UserError
 class ClinicAppointment(models.Model):
     _name = "clinic.appointment"
 
-#    name = fields.Char(string="Name", compute="_compute_name", store=True)
     appointment_id = fields.Char(string='Appointment ID', readonly=True, copy=False)
     patient_id = fields.Many2one("res.partner",string="Patient",required=True)
     doctor_id = fields.Many2one("res.users",string="Doctor",required=True)
@@ -25,17 +24,21 @@ class ClinicAppointment(models.Model):
     notes = fields.Text(string="Add notes")
     
     @api.model
-    def create(self, vals):  
-
-        if vals.get('appointment_id'):
+    def create(self, vals):
+        if not vals.get('appointment_id'):
             vals['appointment_id'] = self.env['ir.sequence'].next_by_code('clinic.appointment.sequence')
         
-        return super(ClinicAppointment, self).create(vals)
-    
-    # @api.depends('appointment_id')
-    # def _compute_name(self):
-    #     for record in self:
-    #         record.name = record.appointment_id
+        appointment = super(ClinicAppointment, self).create(vals)
+        
+        appointment.log_id.create({
+            'patient_id': appointment.patient_id.id,
+            'appointment_id': appointment.id,
+            'create_uid': appointment.create_uid.id,
+            'entry_datetime': appointment.appointment,
+            'notes': appointment.notes
+        })
+        
+        return appointment
     
     @api.constrains
     def _check_conflict(self):
@@ -68,14 +71,13 @@ class ClinicAppointment(models.Model):
         })
         return super().pay_action()
 
-    @api.model
-    def create(self, vals):
-        res = super(ClinicAppointment, self).create(vals)
-        res.log_id.create({
-            'patient_id': res.patient_id.id,
-            'appointment_id': res.id,
-            'create_uid': res.create_uid,
-            'entry_datetime': res.appointment,
-            'notes': res.notes
-        })
-        return res    
+    # @api.model
+    # def create(self, vals):    
+    #     self.log_id.create({
+    #         'patient_id': self.patient_id.id,
+    #         'appointment_id': self.id,
+    #         'create_uid': self.create_uid,
+    #         'entry_datetime': self.appointment,
+    #         'notes': self.notes
+    #     })
+    #     return super().create(vals)    
