2ac538bd79e714cd41a1c5b1eb189c236cfc8d40
muhammad_wael <mwael1971@gamil.com>
2024-07-08 15:04:13 +0300

    AVAILABILITYY DOOOOOOOOONE

--- models/clinic_doctor.py
+++ models/clinic_doctor.py
@@ -1,4 +1,5 @@
 from odoo import models,fields,api
+from datetime import timedelta,datetime
 
 class ClinicDoctor(models.Model):
     _inherit = "res.users"
@@ -12,18 +13,7 @@ class ClinicDoctor(models.Model):
     string="Upcoming Appointments"
     )
 
-    @api.depends('appointment_id')
-    def _compute_avalability(self):
-        for record in self:
-            unavailable = self.env['clinic.appointment'].search([
-                ('doctor_id', '=', record.id),
-                ('status', 'in', ['confirmed', 'pending'])
-            ])
-            if unavailable:
-                record.availability = 'unavailable'
-            else:
-                record.availability = 'available'
-        
+    
     @api.depends('appointment_id')
     def _compute_upcoming_appointments(self):
         for record in self:
@@ -33,3 +23,33 @@ class ClinicDoctor(models.Model):
                 ('status', 'in', ['confirmed', 'pending'])
             ])
             record.upcoming_appointments = upcoming_appointments
+    
+    @api.onchange('availability')
+    def _make_appointments(self):
+        day_mapping = {
+            'sat': 5,
+            'sun': 6,
+            'mon': 0,
+            'tues': 1,
+            'wed': 2,
+            'thurs': 3,
+            'fri': 4
+        }
+        for record in self:
+            for availability in record.availability:
+                current_date = datetime.now()
+                end_date = current_date + timedelta(weeks=4)
+                
+                while current_date <= end_date:
+                    if current_date.weekday() == day_mapping.get(availability.week_day.strip().lower()):
+                        start_time = availability.start_time
+                        end_time = availability.end_time
+                        while start_time < end_time:
+                            appointment_datetime = datetime.combine(current_date, datetime.min.time()) + timedelta(hours=start_time)
+                            self.env['clinic.appointment'].create({
+                                'doctor_id': record._origin.id, #because onchange make new (temp) values and we need to use original ones 
+                                'appointment': appointment_datetime,
+                                'status': 'available'
+                            })
+                            start_time +=0.25
+                    current_date += timedelta(days=1)
