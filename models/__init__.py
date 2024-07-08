80450dd703c9389f17eabab6588da2e4c0ddb2de
muhammad_wael <mwael1971@gamil.com>
2024-07-08 10:00:47 +0300

    Add doctor availability

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from . import clinic_patient
 from . import clinic_doctor
 from . import clinic_appointment
+from . import clinic_availability
 from . import clinic_treatment
 from . import clinic_medical_record
 from . import clinic_prescription
